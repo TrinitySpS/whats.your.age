f5e3423d769fc3be68280c54b7710bb4a93dfc9d
trinity-senpai <45470217+TrinitySpS@users.noreply.github.com>
2020-03-21 15:18:10 +0200

    Create wya.py

---
+++ wya.py
@@ -0,0 +1,8 @@
+print('Guess date of birth by \n TrinitySenpai ♡ \n')
+
+
+userInput = int(input('Enter year of birth  \n :'))
+
+age = 2020-userInput
+
+print("\n It seems you're " + str(age) + " years old...right?")
